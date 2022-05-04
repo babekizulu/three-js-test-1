2ab4806f0caeec32633474a60020a548e8be3ec3
BABEKIZULU <babekizulu@gmail.com>
2022-05-04 08:09:22 +0200

    Fixed Module Import Issues

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import { render } from 'node-sass';
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 const canvas = document.querySelector('#canvas');
 const {WebGLRenderer, PerspectiveCamera, Scene, BoxGeometry, MeshBasicMaterial, Mesh} = THREE;
 
